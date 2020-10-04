4ec96768f92476c3d80648287f0d65d1557ebbe7
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2020-10-03 23:53:26 -0300

    Fixed algorithm

--- dijkstra/dijkstra.lisp
+++ dijkstra/dijkstra.lisp
@@ -1,3 +1,5 @@
+#!/usr/bin/sbcl --script
+
 (defparameter *graph* '((a (a b . 7) (a c . 9) (a f . 14))
                         (b (b c . 10) (b d . 15))
                         (c (c d . 11) (c f . 2))
@@ -5,25 +7,18 @@
                         (e (e f . 9))))
 
 
-(defun list-paths (start)
-  (cdr (assoc start *graph*)))
-
-(defun starts-with (path item)
-  (eql item (first path)))
-
-(defun ends-with (path item)
-  (eql item (first (last path))))
-
 (defun all-paths (start finish &optional (cost 0))
   (loop
-     for section in (list-paths start)
+     for section in (cdr (assoc start *graph*))
      for next-node = (second section)
-     for path = (if (and (not (eql start finish)) (list-paths next-node))
-                    (cons start (first (all-paths next-node finish (+ cost (cddr section)))))
-                    (cons start cost))
-     if (and (starts-with path start) (ends-with path finish))
-     collect path))
+     for next-cost = (+ cost (cddr section))
+     for path = (if (eql next-node finish)
+                    (cons start (cons finish next-cost))
+                    (let ((recurse (first (all-paths next-node finish next-cost))))
+                      (when recurse (cons start recurse))))
+     if path collect path))
+
 
 (defun dijkstra-shortest-path (start finish)
-  (flet ((path-cost (x) (cdr (last x))))
-    (first (sort (all-paths start finish) #'< :key #'path-cost))))
+  (first (sort (all-paths start finish) #'< :key (lambda (x) (cdr (last x))))))
+
