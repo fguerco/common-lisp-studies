57d3c2ba69d7e0eb1155f4e0d612916f308c76c9
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2020-10-04 14:56:09 -0300

    Small refactoring

--- 99-bottles/99bottles.lisp
+++ 99-bottles/99bottles.lisp
@@ -1,11 +1,13 @@
+#!/usr/bin/sbcl --script
 ;; simple
 
 (defun bottles (bot &optional (output t))
-  (format output "~%~@(~r~) bottle~:p of beer on the wall~%" bot)
-  (format output "~@(~r~) bottle~:p of beer~%" bot)
-  (format output "Take one down, pass it around~%")
-  (format output "~@(~r~) bottle~:p of beer~%" (1- bot))
-  (when (> bot 1) (bottles (1- bot) output)))
+  (when (> bot 0)
+    (progn
+      (format output "~%~@(~r~) bottle~:p of beer on the wall~%" bot)
+      (format output "~@(~r~) bottle~:p of beer~%" bot)
+      (format output "Take one down, pass it around~%")
+      (format output "~@(~r~) bottle~:p of beer~%" (1- bot))
+      (bottles (1- bot) output))))
       
-
 (bottles 99)
