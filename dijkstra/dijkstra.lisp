674b00b13925a6b80632dc5ba2d80c204f474951
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2020-10-04 00:19:30 -0300

    Added shell execution when arguments are given

--- dijkstra/dijkstra.lisp
+++ dijkstra/dijkstra.lisp
@@ -14,11 +14,16 @@
      for next-cost = (+ cost (cddr section))
      for path = (if (eql next-node finish)
                     (cons start (cons finish next-cost))
-                    (let ((recurse (first (all-paths next-node finish next-cost))))
-                      (when recurse (cons start recurse))))
+                    (let ((rest-of-path (first (all-paths next-node finish next-cost))))
+                      (when rest-of-path (cons start rest-of-path))))
      if path collect path))
 
 
 (defun dijkstra-shortest-path (start finish)
   (first (sort (all-paths start finish) #'< :key (lambda (x) (cdr (last x))))))
 
+
+(let ((start (second *posix-argv*))
+      (finish (third *posix-argv*)))
+  (when (and start finish)
+    (format t "~a~%" (dijkstra-shortest-path (read-from-string start) (read-from-string finish)))))
