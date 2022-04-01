902695225106cff02c853d643bef42c0b578e590
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2022-03-31 21:34:48 -0400

    Improved printing

--- 99-bottles/99bottles.lisp
+++ 99-bottles/99bottles.lisp
@@ -1,13 +1,11 @@
-#!/usr/bin/sbcl --script
-;; simple
 
-(defun bottles (bot &optional (output t))
+(defun bottles (&optional (bot 99) (output t))
   (when (> bot 0)
     (progn
       (format output "~%~@(~r~) bottle~:p of beer on the wall~%" bot)
       (format output "~@(~r~) bottle~:p of beer~%" bot)
       (format output "Take one down, pass it around~%")
-      (format output "~@(~r~) bottle~:p of beer~%" (1- bot))
+      (format output "~@(~[no more~:;~:*~r~]~) bottle~:p of beer~%" (1- bot))
       (bottles (1- bot) output))))
       
-(bottles 99)
+(bottles)
