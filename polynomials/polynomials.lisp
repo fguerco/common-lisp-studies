88972e0439455e03c9a22c94fac0fc6cb4aa5920
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2022-03-31 21:24:17 -0400

    Refactor the polynomial term to structs

--- polynomials/polynomials.lisp
+++ polynomials/polynomials.lisp
@@ -1,111 +1,69 @@
-(defun operator? (term)
-  (find term '(+ - * /)))
-
-(defun filter-variable (term)
-  (remove-if (lambda (x) (or (numberp x) (operator? x))) term))
-
-(defun filter-constant (term)
-  (remove-if (lambda (x) (or (not (numberp x)) (operator? x))) term))
-
-
-(defun term-variable (term)
-  (let ((v
-          (cond
-            ((and (atom term) (not (numberp term)) (not (operator? term))) term)
-            ((not (atom term)) (filter-variable term))
-            (t nil))))
-    (if (or (atom v) (cdr v)) v
-        (car v))))
-
-(defun term-coefficient (term)
-  (let ((c (cond
-             ((and (atom term) (numberp term)) term)
-             ((and (atom term) (not (numberp term)) (not (operator? term))) 1)
-             ((not (atom term)) (or (filter-constant term) 1))
-             (t nil))))
-    (if (or (atom c) (cdr c)) c
-        (car c))))
-
-(defun is-term? (term)
-  (or (term-variable term) (term-coefficient term)))
+(defstruct (term (:constructor make-term (const &optional var var-exp)))
+  const var var-exp)
 
 (defun like-terms? (a b)
-  (and a b (equal (term-variable a) (term-variable b))))
+  (equal (list (term-var a) (term-var-exp a))
+         (list (term-var b) (term-var-exp b))))
 
 
 (defun sum-like-terms (a b)
-  (let ((term-variable (term-variable a))
-        (coefficient-a (or (term-coefficient a) 0))
-        (coefficient-b (or (term-coefficient b) 0)))
-    (cond
-      ((null term-variable) (+ coefficient-a coefficient-b))
-      ((listp term-variable) (append (list '* (+ coefficient-a coefficient-b)) term-variable))
-      (t  (list '* (+ coefficient-a coefficient-b) term-variable)))))
-
+  (make-term (+ (term-const a) (term-const b))
+             (term-var a)
+             (term-var-exp a)))
 
 (defun find-like-term (a b)
   (find a b :test #'like-terms?))
 
-(defun remove-operator (polynomial)
-  (cdr polynomial))
-
-
 (defun sum-polynomials (a b)
-  (loop for term-a in (remove-operator a)
-        for term-b = (find-like-term term-a (remove-operator b))
-        if (and (is-term? term-a) (like-terms? term-a term-b))
-          collect (sum-like-terms term-a term-b) into sum
-        finally (return (if sum (cons '+ sum) nil))))
+  (loop for term-a in a
+        for term-b = (find-like-term term-a b)
+        if (like-terms? term-a term-b)
+          collect (sum-like-terms term-a term-b)))
           
+
+(sum-polynomials (list (make-term 2 'x 2) (make-term -10 'x 1) (make-term 5))
+                 (list (make-term -1 'x 2) (make-term 4 'x  1) (make-term 3)))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;                Tests                    ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(deftest test-term-variable ()
-  (check
-    (null (term-variable 2))
-    (eq 'x (term-variable 'x))
-    (eq 'x (term-variable '(* 2 x)))
-    (equal '(x x) (term-variable '(* 10 x x)))))
-
-
-(deftest test-term-coefficient ()
-  (check
-    (= 2 (term-coefficient 2))
-    (= 1 (term-coefficient '(* x x)))
-    (= 2 (term-coefficient '(* 2 x)))
-    (= 1 (term-coefficient 'x))
-    (not (term-coefficient '+))))
-
-
 (deftest test-like-terms? ()
   (check
-    (like-terms? '(* x x) '(* 2 x x))
-    (like-terms? 'x '( * -1 x))
-    (like-terms? '(* -10 x x x) '(x x x))
-    (not (like-terms? '(* 2 x x) 'x))
-    (not (like-terms? '(* 2 x) '(* 2 y)))))
+    (like-terms? (make-term 1 'x 2) (make-term 2 'x 2))
+    (like-terms? (make-term 1 'x) (make-term -1 'x))
+    (like-terms? (make-term -10 'x 3) (make-term 1 'x 3))
+    (not (like-terms? (make-term 2 'x 2) (make-term 1 'x 1)))
+    (not (like-terms? (make-term 2 'x 1) (make-term 2 'y 1)))))
 
 (deftest test-sum-like-terms ()
   (check
-    (= 10 (sum-like-terms 7 3))
-    (equal '(* 2 x) (sum-like-terms 'x 'x))
-    (equal '(* 0 x) (sum-like-terms '(* 1 x) '(* -1 x)))
-    (equal '(* 2 x x) (sum-like-terms '(* x x) '(* x x)))))
+    (equalp (make-term 10) (sum-like-terms (make-term 7) (make-term 3)))
+    (equalp (make-term 2 'x 1) (sum-like-terms (make-term 1 'x 1) (make-term 1 'x 1)))
+    (equalp (make-term 0 'x 1) (sum-like-terms (make-term 1 'x 1) (make-term -1 'x 1)))
+    (equalp (make-term 2 'x 2) (sum-like-terms (make-term 1 'x 2) (make-term 1 'x 2)))))
 
 (deftest test-find-like-term ()
   (check
-    (equal '(* 10 x) (find-like-term 'x '(+ (* 2 x x) (* 10 x) -3)))))
+    (equalp (make-term 10 'x 1) (find-like-term (make-term 1 'x 1)
+                                               (list (make-term 2 'x 2)
+                                                     (make-term 10 'x 1)
+                                                     (make-term -3))))))
 
 (deftest test-sum-polynomials ()
   (check
-    (equal '(+ (* 1 x x) (* -4 x) 13)
-           (sum-polynomials '(+ (* 2 x x) (* -6 x) 5) '(+ (* -1 x x) (* 2 x) 8)))))
+    (equalp (list (make-term 1 'x 2)
+                  (make-term -4 'x 1)
+                  (make-term 13))
+            (sum-polynomials (list (make-term 2 'x 2)
+                                   (make-term -6 'x 1)
+                                   (make-term 5))
+                             (list (make-term -1 'x 2)
+                                   (make-term 2 'x 1)
+                                   (make-term 8))))))
 
 (deftest test-polynomials ()
-  (test-term-variable)
-  (test-term-coefficient)
   (test-like-terms?)
   (test-sum-like-terms)
   (test-find-like-term)
