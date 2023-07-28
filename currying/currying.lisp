57b085b46228d1bcfda4002b60769adfb48eaab7
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2023-07-27 23:25:54 -0400

    added a macro to create curried functions

--- currying/currying.lisp
+++ currying/currying.lisp
@@ -1,3 +1,8 @@
+(defpackage #:currying
+  (:use #:cl))
+
+(in-package #:currying)
+
 (defun auto-curry (fn nargs)
   (when (< nargs 2)
     (error "nargs should be two or more"))
@@ -15,3 +20,17 @@
 
 (defparameter *tri-divide* (auto-curry #'/ 3))
 (funcall (funcall (funcall *tri-divide* 1) 3) 5)
+
+
+(defmacro defcurry (name args &body forms)
+  (unless (>= (length args) 2)
+    (error "should have two or more arguments"))
+  `(defun ,name (,(car args))
+     ,(labels ((add-fn (args)
+                 (if args
+                     `(lambda (,(car args))
+                        ,(add-fn (cdr args)))
+                     `(progn
+                        ,@forms))))
+        (add-fn (cdr args)))))
+
