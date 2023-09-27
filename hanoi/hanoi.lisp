e1b40dbf20b6c93f3a129c6f6f189305082b3a9b
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2023-09-27 00:41:36 -0400

    slight refactoring

--- hanoi/hanoi.lisp
+++ hanoi/hanoi.lisp
@@ -1,10 +1,14 @@
+(defpackage :hanoi
+  (:use :cl))
+
+(in-package :hanoi)
+
 ;; tower of hanoi
 (defun move (n from to spare)
-  (if (= n 0) "done"
-      (progn
-        (move (1- n) from spare to)
-        (format t "moving ~d from ~d to ~d with ~d as spare~%" n from to spare)
-        (move (1- n) spare to from))))
+  (when (plusp n)
+    (move (1- n) from spare to)
+    (format t "moving ~d from ~d to ~d with ~d as spare~%" n from to spare)
+    (move (1- n) spare to from)))
 
 
-(move 3 1 2 3)
+(move 3 'a 'c 'b)
