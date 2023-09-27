135ad7a5257dcf4e6cbe6331cb2f5b7d773c6176
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2023-09-27 00:49:16 -0400

    refactoring on infix code

--- infix.lisp
+++ infix.lisp
@@ -5,19 +5,21 @@
 
 (in-package :infix)
 
-(defun decompose (op expr)
-  (cond ((and (consp expr) (cddr expr))
-         (destructuring-bind (a b c . d) expr
-           (if (member b op)
-               (decompose op (cons (funcall b (calculate a) (calculate c)) d))
-               (cons a (decompose op (cdr expr))))))
-        (t expr)))
-
 (defun calculate (expr)
-  (let ((r (->> expr
-             (decompose '(* /))
-             (decompose '(+ -)))))
-    (if (consp r) (car r) r)))
+  (labels
+      ((decompose (op expr)
+         (if (and (consp expr) (cddr expr))
+             (destructuring-bind (a b c . d) expr
+               (if (find b op)
+                   (decompose op (cons (funcall b (calculate a) (calculate c)) d))
+                   (cons a (decompose op (cdr expr)))))
+             expr)))
+    (if (numberp expr)
+        expr
+        (let ((r (->> expr
+                   (decompose '(* /))
+                   (decompose '(+ -)))))
+          (if (consp r) (car r) r)))))
 
 (defun parse-number (st)
   (read-from-string
@@ -47,9 +49,9 @@
 (defun invert-signals (expr)
   (reduce (lambda (it prev)
             (cond ((consp it) (cons (invert-signals it) prev))
-                  ((and (eql :invert it) (atom (car prev))) (list* -1 '* prev))
+                  ((and (eql :invert it) (atom (car prev))) `(-1 * ,@prev))
                   ((and (eql :invert it) (consp (car prev)))
-                   (list (list -1 '* (car prev))))
+                   `((0 - ,(car prev))))
                   (t (cons it prev))))
           expr :from-end t :initial-value nil))
 
@@ -58,65 +60,57 @@
         ((and (consp expr) (null (cdr expr))) (car expr))
         (t expr)))
 
-(defmethod infix ((st stream))
-  (-> st
-    read-terms
-    invert-signals
-    normalize-list
-    calculate))
-
-(defmethod infix ((expr string))
+(defun infix (expr)
   (with-input-from-string (st expr)
-    (infix st)))
-
+    (-> st
+      read-terms
+      invert-signals
+      normalize-list
+      calculate)))
 
 ;;;; tests
 
 (defmacro make-assertions (test-data)
   `(progn
-     ,@(mapalist (lambda (expr expected)
-                   `(is = ,expected (infix ,expr)))
-                 test-data)))
+     ,@(loop for (expr expected) in test-data
+             collect `(is = ,expected (infix ,expr)))))
                 
 
 (define-test infix-tests ()
-  (make-assertions (("-123" . -123)
-                    ("12 * -1" . -12)
-                    ("1 - 1" . 0)
-                    ("4 - 1" . 3)
-                    ("1 - -1" . 2)
-                    ("1 + 2 - 3 * (4 / 6)" . 1)
-                    ("(1 + 2) - 3 * (4 / 6)" . 1)
-                    ("12 * 123 / (-5 + 2)" . -492)
-                    ("(1 + 2 + 3) * 10 / ((2 * 3) + 3)" . 20/3)
-                    ("((1 + 2) - 3) * (4 / 6)" . 0)
-                    ("((1 + 2) * (1 - (1 + 2))) + (((10 - 1) * 2) + 3)" . 15)
-                    ("2 + 3" . 5)
-                    ("2 + 3 / 4" . 11/4)
-                    ("2 * 3 - 4" . 2)
-                    ("2 * (3 + (4 * 5 + (6 * 7) * 8) - 9) * 10" . 7000)
-                    ("2 * -3 - -4 + -0.25" . -2.25)
-                    ("12*-1" . -12)
-                    ("1-1" . 0)
-                    ("4-1" . 3)
-                    ("1 -1" . 0)
-                    ("1 - 1" . 0)
-                    ("1- -1" . 2)
-                    ("13 + 112" . 125)
-                    ("325 + 1124" . 1449)
-                    ("5 + 11 - 4" . 12)
-                    ("5 * 11 - 20 + 5" . 40)
-                    ("1 + 2 - 3 * (4 / 6)" . 1)
-                    ("(1 + 2) - 3 * (4 / 6)" . 1)
-                    ("((1 + 2) - 3) * (4 / 6)" . 0)
-                    ("-123" . -123)
-                    ("12*-1" . -12)
-                    ("12* 123/-(-5 + 2)" . 492)
-                    ("(-5 + 2)" . -3)
-                    ("-(-5 + 2)" . 3)
-                    ("(3 + 2)" . 5)
-                    ("(3 + 5) * 10" . 80)
-                    ("12* 123/(-5 + 2)" . -492))))
+  (make-assertions (("-123" -123)
+                    ("12 * -1" -12)
+                    ("12*-1" -12)
+                    ("1 - 1" 0)
+                    ("1-1" 0)
+                    ("1 -1" 0)
+                    ("4 - 1" 3)
+                    ("4-1" 3)
+                    ("1 - -1" 2)
+                    ("1- -1" 2)
+                    ("1 + 2 - 3 * (4 / 6)" 1)
+                    ("(1 + 2) - 3 * (4 / 6)" 1)
+                    ("12 * 123 / (-5 + 2)" -492)
+                    ("12* 123/(-5 + 2)" -492)
+                    ("(1 + 2 + 3) * 10 / ((2 * 3) + 3)" 20/3)
+                    ("((1 + 2) - 3) * (4 / 6)" 0)
+                    ("((1 + 2) * (1 - (1 + 2))) + (((10 - 1) * 2) + 3)" 15)
+                    ("2 + 3" 5)
+                    ("2 + 3 / 4" 11/4)
+                    ("2 * 3 - 4" 2)
+                    ("2 * (3 + (4 * 5 + (6 * 7) * 8) - 9) * 10" 7000)
+                    ("2 * -3 - -4 + -0.25" -2.25)
+                    ("13 + 112" 125)
+                    ("325 + 1124" 1449)
+                    ("5 + 11 - 4" 12)
+                    ("5 * 11 - 20 + 5" 40)
+                    ("1 + 2 - 3 * (4 / 6)" 1)
+                    ("(1 + 2) - 3 * (4 / 6)" 1)
+                    ("((1 + 2) - 3) * (4 / 6)" 0)
+                    ("12* 123/-(-5 + 2)" 492)
+                    ("(-5 + 2)" -3)
+                    ("-(-5 + 2)" 3)
+                    ("(3 + 2)" 5)
+                    ("(3 + 5) * 10" 80))))
 
 (test :infix)
 
