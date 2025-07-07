d0dd693f92cdd6019c0f613b9a3bf686bf1d2f83
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2025-07-07 10:34:54 -0400

    combinator update

--- combinator.lisp
+++ combinator.lisp
@@ -1,24 +1,36 @@
 ;; Y combinator as Lambda abstraction for lazy evaluation languages
 (defun y (f)
-  ((lambda (x)
+  (funcall
+   (lambda (x)
      (funcall f (funcall x x)))
    (lambda (x)
      (funcall f (funcall x x)))))
 
 ;; this is to prevent infinite recursion
-(defun y (f)
-  ((lambda (x) (funcall x x))
+(defun y* (f)
+  (funcall
+   (lambda (x) (funcall x x))
    (lambda (x)
      (funcall f (lambda (&rest args)
                   (apply (funcall x x) args))))))
-     
 
-(y (lambda (f)
-     (lambda (n)
-       (if (zerop n) 1
-           (* n (funcall f (1- n)))))))
+;; the Z combinator also for eager languages
+(defun z (f)
+  (funcall
+   (lambda (x)
+     (funcall f (lambda (v) (funcall (funcall x x) v))))
+   (lambda (x)
+     (funcall f (lambda (v) (funcall (funcall x x) v))))))
 
-;; long version without defun
+
+;;; the factorial function to run recursively
+(defparameter fac
+  (lambda (f)
+    (lambda (n)
+      (if (zerop n) 1
+          (* n (funcall f (1- n)))))))
+
+;; long version without defun using y*
 ((lambda (f)
    ((lambda (x) (funcall x x))
     (lambda (x)
